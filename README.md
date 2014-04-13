dd7282f8794e48b4f6466ce99268ccb195c02a0e
You should know <contact.me@directly>
2014-04-14 00:54:34 +0300

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ca
 ==
 
 Certificate Authority
+
+Quickstart for creating a Certificate Authority with OpenSSL with relatively sane defaults.
---
+++ aliases.sh
@@ -0,0 +1,4 @@
+# 'source aliases.sh' for aliases
+alias cert="openssl x509 -noout -text -in"
+alias req="openssl req -noout -text -in"
+alias crl="openssl crl -noout -text -in"
